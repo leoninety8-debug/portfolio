2546fb8c6214acbad9460efe37e172e2735176e5
leoninety8-debug <leoninety8@gmail.com>
2026-07-01 16:21:13 +0700

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/leoninety8-debug/Portfolio_Fashli.git',
+  base: '/portfolio/',
   plugins: [react()],
 })
